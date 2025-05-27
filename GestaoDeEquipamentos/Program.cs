8864fa7e49894d7146258f4374344da9d2e6e637
Marlon Quilante <marlonquilante@gmail.com>
2025-05-27 20:20:31 -0300

    Implementado polimorfismo nas telas da classe Program

--- GestaoDeEquipamentos/Program.cs
+++ GestaoDeEquipamentos/Program.cs
@@ -7,144 +7,44 @@ namespace GestaoDeEquipamentos
     {
         static void Main(string[] args)
         {
-            ProductController productController = new ProductController();
-            ProductView productView = new ProductView(productController);
-
-            ManufactorController manufactorController = new ManufactorController();
-            ManufactorView manufactorView = new ManufactorView(manufactorController);
-
-            TicketController ticketController = new TicketController();
-            TicketView ticketView = new TicketView(ticketController);
-
-            manufactorController.productController = productController;
-
-            productView.manufactorController = manufactorController;
-            productView.manufactorView = manufactorView;
-
-            ticketView.productController = productController;
-            ticketView.productView = productView;
-
             bool systemON = true;
+
+            MainView mainView = new MainView();
 
             while (systemON == true)
             {
-                MainHeader();
-                string option = MainMenu();
+                mainView.MainHeader();
+                mainView.MainMenu();
+                BaseView chosenView = mainView.GetView();
 
-                switch (int.Parse(option)) 
+                if (chosenView == null)
                 {
-                    case 1:
-                        ProductOptions(productView);
-                        break;
-                    case 2:
-                        TicketOptions(ticketView);
-                        break;
-                    case 3:
-                        ManufactorOptions(manufactorView);
-                        break;
-                    case 4:
-                        systemON = false;
-                        break;
-                    default:
-                        break;
-                } 
-            }
-        }
+                    systemON = false;
+                    break;
+                }
+                else
+                {
+                    chosenView.MainHeader();
+                    string option = chosenView.Menu();
 
-        static void MainHeader()
-        {
-            Console.Clear();
-            Console.WriteLine("-----------------");
-            Console.WriteLine("SISTEMA DE GESTÃO");
-            Console.WriteLine("-----------------");
-        }
-
-        static string MainMenu()
-        {
-            Console.WriteLine("\nSelecione uma opção...\n");
-            Console.WriteLine("1- Equipamentos");
-            Console.WriteLine("2- Chamados");
-            Console.WriteLine("3- Fabricantes");
-            Console.WriteLine("4- Sair\n");
-
-            return Console.ReadLine();
-        }
-
-        static void ProductOptions(ProductView productView)
-        {
-            productView.MainHeader();
-            string productOption = productView.Menu();
-
-            switch (int.Parse(productOption))
-            {
-                case 1:
-                    productView.Create();
-                    break;
-                case 2:
-                    productView.Read();
-                    break;
-                case 3:
-                    productView.Update();
-                    break;
-                case 4:
-                    productView.Delete();
-                    break;
-                case 5:
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        static void TicketOptions(TicketView ticketView)
-        {
-            ticketView.MainHeader();
-            string ticketOption = ticketView.Menu();
-
-            switch (int.Parse(ticketOption))
-            {
-                case 1:
-                    ticketView.Create();
-                    break;
-                case 2:
-                    ticketView.Read();
-                    break;
-                case 3:
-                    ticketView.Update();
-                    break;
-                case 4:
-                    ticketView.Delete();
-                    break;
-                case 5:
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        static void ManufactorOptions(ManufactorView manufactorView)
-        {
-            manufactorView.MainHeader();
-            string manufactorOption = manufactorView.Menu();
-
-            switch (int.Parse(manufactorOption))
-            {
-                case 1:
-                    manufactorView.Create();
-                    break;
-                case 2:
-                    manufactorView.Read();
-                    break;
-                case 3:
-                    manufactorView.Update();
-                    break;
-                case 4:
-                    manufactorView.Delete();
-                    break;
-                case 5:
-                    break;
-                default:
-                    break;
+                    switch (int.Parse(option))
+                    {
+                        case 1:
+                            chosenView.Create();
+                            break;
+                        case 2:
+                            chosenView.Read();
+                            break;
+                        case 3:
+                            chosenView.Update();
+                            break;
+                        case 4:
+                            chosenView.Delete();
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         }
     }
---
+++ GestaoDeEquipamentos/View/MainView.cs
@@ -0,0 +1,67 @@
+﻿using GestaoDeEquipamentos.Controller;
+
+namespace GestaoDeEquipamentos.View
+{
+    public class MainView
+    {
+        private char option;
+
+        private ManufactorController manufactorController;
+        private ProductController productController;
+        private TicketController ticketController;
+
+        private ManufactorView manufactorView;
+        private ProductView productView;
+        private TicketView ticketView;
+
+        public MainView()
+        {
+            manufactorController = new ManufactorController();
+            productController = new ProductController();
+            ticketController = new TicketController();
+
+            manufactorView = new ManufactorView(manufactorController);
+            productView = new ProductView(productController);
+            ticketView = new TicketView(ticketController);
+
+            manufactorController.productController = productController;
+            
+            productView.manufactorController = manufactorController;
+            productView.manufactorView = manufactorView;
+
+            ticketView.productView = productView;
+            ticketView.productController = productController;
+        }
+
+        public void MainHeader()
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------");
+            Console.WriteLine("SISTEMA DE GESTÃO");
+            Console.WriteLine("-----------------");
+        }
+
+        public void MainMenu()
+        {
+            Console.WriteLine("\nSelecione uma opção...\n");
+            Console.WriteLine("1- Equipamentos");
+            Console.WriteLine("2- Chamados");
+            Console.WriteLine("3- Fabricantes");
+            Console.WriteLine("4- Sair\n");
+
+            option = Console.ReadLine()[0];
+        }
+
+        public BaseView GetView()
+        {
+            if (option == '1')
+                return productView;
+            else if (option == '2')
+                return ticketView;
+            else if (option == '3')
+                return manufactorView;
+
+            return null;
+        }
+    }
+}
