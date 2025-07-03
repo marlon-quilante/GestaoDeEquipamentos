3a2583074ee0a8d405010e5c61b63ede7a75ee84
Marlon Quilante <marlonquilante@gmail.com>
2025-07-03 20:11:23 -0300

    Iniciado a implementação do HomeController

---
+++ GestaoDeEquipamentos.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentos.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Ação
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
