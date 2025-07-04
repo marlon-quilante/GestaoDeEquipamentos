ce7ea8ddd42d525f920b4d37e178f6f44de3b7ff
Marlon Quilante <marlonquilante@gmail.com>
2025-07-03 23:34:49 -0300

    Implementado o cadastro e visualização de fabricantes

---
+++ GestaoDeEquipamentos.WebApp/Controllers/FabricanteController.cs
@@ -0,0 +1,41 @@
+﻿using GestaoDeEquipamentos.Dominio;
+using GestaoDeEquipamentos.Infraestrutura.Arquivos;
+using GestaoDeEquipamentos.Infraestrutura.Arquivos.Compartilhado;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentos.WebApp.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private RepositorioFabricanteEmArquivo repositoryManufactor;
+
+        public FabricanteController()
+        {
+            ContextoDados context = new ContextoDados(true);
+            repositoryManufactor = new RepositorioFabricanteEmArquivo(context);
+        }
+
+        public IActionResult Index()
+        {
+            List<Fabricante> manufactors = repositoryManufactor.GetRegisters();
+
+            return View(manufactors);
+        }
+
+        [HttpGet] //Opcional, pois o framework entende que é GET se deixar sem o atributo
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(string name, string email, string phone)
+        {
+            Fabricante newManufactor = new Fabricante(name, email, phone);
+
+            repositoryManufactor.Create(newManufactor);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
