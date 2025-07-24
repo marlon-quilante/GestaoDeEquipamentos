b4dc7818817a0e504991cd95a88d12e538f60176
Marlon Quilante <marlonquilante@gmail.com>
2025-07-24 00:41:28 -0300

    Implementado alteração de modo escuro e modo claro

---
+++ GestaoDeEquipamentos.WebApp/wwwroot/AlterarTema.js
@@ -0,0 +1,27 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const html = document.documentElement;
+
+    const botaoTemaEscuro = document.getElementById("temaEscuro");
+    const botaoTemaClaro = document.getElementById("temaClaro");
+
+    const temaSalvo = localStorage.getItem("tema");
+    if (temaSalvo) {
+        html.setAttribute("data-bs-theme", temaSalvo);
+    }
+
+    if (botaoTemaEscuro) {
+        botaoTemaEscuro.addEventListener("click", function () {
+            const temaEscuro = "dark";
+            html.setAttribute("data-bs-theme", temaEscuro);
+            localStorage.setItem("tema", temaEscuro);
+        });
+    }
+
+    if (botaoTemaClaro) {
+        botaoTemaClaro.addEventListener("click", function () {
+            const temaClaro = "light";
+            html.setAttribute("data-bs-theme", temaClaro);
+            localStorage.setItem("tema", temaClaro);
+        });
+    }
+})
